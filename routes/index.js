1a770106146245fc59884eb8b6e3511d6dd5e9e9
RyanLam22 <ryanlam5784@gmai.com>
2023-10-02 19:37:18 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* GET about me page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
-
+/* GET project page. */
 router.get('/project', function(req, res, next) {
   res.render('project', { title: 'Project' });
 });
-
+/* GET service page. */
 router.get('/service', function(req, res, next) {
   res.render('service', { title: 'Service' });
 });
+/* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
